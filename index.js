72e88ff9406de4982e0db891073de49ae65bdd91
Gavin Sullivan <gavintsullivan@gmail.com>
2023-03-04 12:11:38 -0800

    Updated code to pass all tests

--- index.js
+++ index.js
@@ -1,22 +1,57 @@
-class Vehicle {
+//DO all your code here!
+
+//Base Class Vehicle
+class Vehicle{
     constructor(model){
-      this.model = model
+        this.model = model
     }
     drive(){
-      return this.model + " " + "drives"
+        return `${this.model} drives`
     }
-  }
-            
+}
 
-  
 class Sedan extends Vehicle{
-    constructor(){}
-}  
+    constructor(model, backupCamera){
+        super(model)
+        this.model = model
+        this.backupCamera = backupCamera
+    }
+    drive(){
+        return `${this.model} drives`
+    }
+}
 
-class SUV extends Vehicle{
-    constructor(){}
-} 
+class Suv extends Vehicle{
+    constructor(model,offroadPackage){
+        super(model)
+        this.model = model
+        this.offroadPackage = offroadPackage
+    }
+    drive(){
+        return `${this.model} drives`
+    }
+}
 
 class Truck extends Vehicle{
-    constructor(){}
-} 
+    constructor(model,towingCapacity){
+        super(model)
+        this.model = model
+        this.towingCapacity = towingCapacity
+    }
+    drive(){
+        return `${this.model} drives`
+    }
+}
+
+class EVSedan extends Vehicle{
+    constructor(model){
+        super(model)
+        this.model = model
+    }
+    drive(){
+        return `${this.model} drives`
+    }
+    recharge(){
+        return `${this.model} recharges`
+    }
+}
